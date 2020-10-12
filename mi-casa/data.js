a403494933adeda3f2e24ba6ef9c78290cb8a2cd
Josep Miquel Manzanera Roma <josep@lengova.com>
2020-10-12 13:21:47 +0200

    Testing mi casa

---
+++ mi-casa/data.js
@@ -0,0 +1,231 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pasillo",
+      "name": "Pasillo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.350002939930139,
+        "pitch": 0.09755711720726978,
+        "fov": 1.4327506036911004
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7696935798073916,
+          "pitch": 0.2403858378244479,
+          "rotation": 0,
+          "target": "4-comedor-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-cocina",
+      "name": "Cocina",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.2892866496518636,
+        "pitch": -0.013332345742835372,
+        "fov": 1.4327506036911004
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5644451625665212,
+          "pitch": 0.18910415767496858,
+          "rotation": 0,
+          "target": "4-comedor-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-comedor-3",
+      "name": "Comedor 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.9367407872302422,
+        "pitch": 0.021041731162352306,
+        "fov": 1.4327506036911004
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2291140107435723,
+          "pitch": 0.16102507958803258,
+          "rotation": 0,
+          "target": "3-comedor-2"
+        },
+        {
+          "yaw": 3.056084500030643,
+          "pitch": 0.09159393705115981,
+          "rotation": 0,
+          "target": "4-comedor-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-comedor-2",
+      "name": "Comedor 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.40201140891574383,
+        "pitch": 0.12051173302074503,
+        "fov": 1.4327506036911004
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.303999371591427,
+          "pitch": 0.1930475339563671,
+          "rotation": 0,
+          "target": "4-comedor-1"
+        },
+        {
+          "yaw": -0.40163625810204273,
+          "pitch": 0.08112054927618217,
+          "rotation": 0,
+          "target": "2-comedor-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-comedor-1",
+      "name": "Comedor 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.8179114151578162,
+        "pitch": 0.18839791126724847,
+        "fov": 1.4327506036911004
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8179114151578162,
+          "pitch": 0.18839791126724847,
+          "rotation": 0,
+          "target": "3-comedor-2"
+        },
+        {
+          "yaw": -0.9732949355316709,
+          "pitch": 0.09678100521544408,
+          "rotation": 0,
+          "target": "2-comedor-3"
+        },
+        {
+          "yaw": 1.4525322693254141,
+          "pitch": 0.05040462256320666,
+          "rotation": 0,
+          "target": "0-pasillo"
+        },
+        {
+          "yaw": 1.7324653387699547,
+          "pitch": 0.09339654527836849,
+          "rotation": 0,
+          "target": "1-cocina"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Mi casa",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
